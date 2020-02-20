7b4342e455b00feffc9c4c6e6d976ac1d4b2df85
MameHub <61141429+MameHub@users.noreply.github.com>
2020-02-20 23:49:50 +0100

    caracol

--- Array/trabajoArray2/Program.cs
+++ Array/trabajoArray2/Program.cs
@@ -8,22 +8,22 @@ namespace trabajoArray2
         {
             // Le pedimos al usuario que rellene un array de números y luego trasladamos los datos de ese array a otra array
 
-            Console.Write("¿Cuantos números quieres introducir?: ");
-            int cantidad = int.Parse(Console.ReadLine());
-            int[] array = new int[cantidad];
-            
-            for (int i = 0; i < array.Length; i++)
+            Console.Write("¿Cuántas veces quieres introducir un número?: ");
+            int repetitions = int.Parse(Console.ReadLine());
+            int[] numbers = new int[repetitions];
+            for (int i = 0; i < numbers.Length; i++)
             {
                 Console.Write("Introduce un número: ");
-                int number = int.Parse(Console.ReadLine());
-                array[i] = number;
+                int x = int.Parse(Console.ReadLine());
+                numbers[i] = x;
             }
-            int[] cantidad2 = new int[array.Length];
-            for (int i = 0; i < array.Length; i++)
+            int[] numbers2 = new int[numbers.Length];
+            for (int i = 0; i < numbers2.Length; i++)
             {
-                
+                numbers2[i] = numbers[i];
+                Console.WriteLine(numbers2[i]);
             }
-
+            // Foto pizarra clase, ejercicio resuelto por correción de Nacho
         }
     }
 }
--- Bucles/ejercicioBuclesAnidados1/Program.cs
+++ Bucles/ejercicioBuclesAnidados1/Program.cs
@@ -8,7 +8,13 @@ namespace ejercicioBuclesAnidados1
         {
             // Ejercicio con bucles anidados, asignaremos datos numéricos a una array y despues ordenaremos estos con un bucle anidado.
 
+            Console.Write("Introduce la cantidad de números que quieres sumar: ");
+            int numberCount = int.Parse(Console.ReadLine());
+            int[] array1 = new int[numberCount];
+            for (int i = 0; i < array1.Length; i++)
+            {
 
+            }
         }
     }
 }
