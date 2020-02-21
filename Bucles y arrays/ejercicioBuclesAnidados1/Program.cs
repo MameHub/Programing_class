c17d29f6afffb7accc4fd0e0833466dde15432c0
MameHub <61141429+MameHub@users.noreply.github.com>
2020-02-21 09:40:57 +0100

    corrección

--- Bucles y arrays/ejercicioBuclesAnidados1/Program.cs
+++ Bucles y arrays/ejercicioBuclesAnidados1/Program.cs
@@ -7,13 +7,20 @@ namespace ejercicioBuclesAnidados1
         static void Main(string[] args)
         {
             // Ejercicio con bucles anidados, asignaremos datos numéricos a una array y despues ordenaremos estos con un bucle anidado.
-
-            Console.Write("Introduce la cantidad de números que quieres sumar: ");
-            int numberCount = int.Parse(Console.ReadLine());
-            int[] array1 = new int[numberCount];
-            for (int i = 0; i < array1.Length; i++)
+            // Copiado directamente de pizarra hecho por profe Nacho
+            int[] numbers = new int[] { 3, 1, 2};
+            for (int i = 0; i < numbers.Length; i++)
             {
-
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[i])
+                    {
+                        int aux = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = aux;
+                    }
+                }
+                Console.WriteLine(numbers[i]);
             }
         }
     }
