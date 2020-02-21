db7de66d2733a0f77b2143e37fb297a41cee2c34
MameHub <61141429+MameHub@users.noreply.github.com>
2020-02-21 10:02:49 +0100

    petada cabeza

---
+++ Bucles y arrays/ejercicioBuclesAnidados2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ejercicioBuclesAnidados2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce el número de filas que quieras: ");
+            int filas = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
