bb4b0bff1f7dae4edb4d4b68254aadbc6a83c74d
MameHub <61141429+MameHub@users.noreply.github.com>
2020-02-21 09:23:45 +0100

    repaso

---
+++ ejercicioRepaso/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ejercicioArrays3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Le pedimos al usuario dos números y los almacenamos, a continuación intercambiamos el valor de las variables.
+
+            int x = 4;
+            int y = 3;
+            int aux = x;
+
+            x = y;
+
+            Console.WriteLine("El valor de x es el siguiente: " + x + " El valor de y es el siguiente: " + y);
+        }
+    }
+}
