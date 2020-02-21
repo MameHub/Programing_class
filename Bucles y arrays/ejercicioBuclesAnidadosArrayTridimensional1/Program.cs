c0777dc5200d0d6539491588f73eea28a5e7e50c
MameHub <61141429+MameHub@users.noreply.github.com>
2020-02-21 12:46:11 +0100

    smoke

---
+++ Bucles y arrays/ejercicioBuclesAnidadosArrayTridimensional1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ejercicioBuclesAnidadosArrayTridimensional1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear un array bidimensional, con los valores ya establecidos o haciendo al usuario que los introduzca, con los siguientes valores 0,0 = 1, 1,1 = 1, 2,2 = 1, el resto 0.
+            Console.Write("Introduce el número de filas que quieres que tenga el array: ");
+            int magnitude = int.Parse(Console.ReadLine());
+            int[,] array = new int[magnitude, magnitude];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        array[i, j] = 1;
+                    }
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
