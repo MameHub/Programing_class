c8ea8e497d59f7ff0603df75a69619ec4799212f
MameHub <61141429+MameHub@users.noreply.github.com>
2020-03-02 00:01:50 +0100

    temp

--- Ejercicios csharp_master/Nivel 2/ConditionalStatements/ConditionalStatements6/Program.cs
+++ Ejercicios csharp_master/Nivel 2/ConditionalStatements/ConditionalStatements6/Program.cs
@@ -6,7 +6,31 @@ namespace ConditionalStatements6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //El usuario introduce una temperatura y nosotros le diremos si está helado, muy frío, normal, caliente o muy caliente.
+
+            Console.Write("Introduzca la temperatura: ");
+            int temp = int.Parse(Console.ReadLine());
+
+            if (temp <= 0)
+            {
+                Console.WriteLine("Helado.");
+            }
+            else if (temp >= 0 && temp <= 10)
+            {
+                Console.WriteLine("Muy frío.");
+            }
+            else if (temp > 10 && temp <= 20)
+            {
+                Console.WriteLine("Normal.");
+            }
+            else if (temp > 20 && temp <= 30)
+            {
+                Console.WriteLine("Caliente.");
+            }
+            else
+            {
+                Console.WriteLine("Muy caliente.");
+            }
         }
     }
 }
