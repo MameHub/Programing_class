35d5b083bec527358f0a8a84d4720d7946e13a55
MameHub <61141429+MameHub@users.noreply.github.com>
2020-03-01 20:46:35 +0100

    variablesPrint_master

---
+++ Ejercicios csharp_master/Nivel 1/VariablesPrint/variablesPrint_master3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace variablesPrint_master3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mostrar el resultado de distintas operaciones.
+
+            Console.WriteLine("Operaciones");
+            Console.WriteLine(-8 + 9 * 13);
+            Console.WriteLine(23 * 2 % 6);
+            Console.WriteLine(80 - 139 * 8);
+            Console.WriteLine(9 * 13 / 2 + 320);
+            Console.WriteLine("Fin de las operaciones");
+        }
+    }
+}
---
+++ Ejercicios csharp_master/Nivel 1/VariablesPrint/variablesPrint_master4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace variablesPrint_master4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mostrar en pantalla el resultado de sumar, restar, multiplicar, dividir y resto de dos números.
+
+            Console.Write("Introduzca el primer número: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Introduzca el segundo número: ");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("El resultado de la suma es: " + (x + y));
+            Console.WriteLine("El resultado de la resta es: " + (x - y));
+            Console.WriteLine("El resultado de la multiplicación es: " + (x * y));
+            Console.WriteLine("El resultado de la división es: " + (x / y));
+            Console.WriteLine("El resto de la división es: " + (x % y));
+
+        }
+    }
+}
