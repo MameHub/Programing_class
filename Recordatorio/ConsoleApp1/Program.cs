1ebfbf4689e35854bf3e4728a2c0039e537c1a2c
MameHub <61141429+MameHub@users.noreply.github.com>
2020-03-09 12:59:59 +0100

    recordatorio

---
+++ Recordatorio/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Le mostramos en pantalla la cantidad de veces que se repiten los números introducidos en una lista.
+
+            int[] numeros = new int[5] { 3, 5, 3, 1, 5 };
+
+        }
+    }
+}
