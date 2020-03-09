9f30c6fdc26ae597e2f98e312db94a0cd14c06e2
MameHub <61141429+MameHub@users.noreply.github.com>
2020-03-09 14:14:41 +0100

    ...

---
+++ Profesor/ParImpar2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParImpar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listaPar = new List<int>();
+            List<int> listaImpar = new List<int>();
+            Console.Write("¿Cuántos números quieres introducir?: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Introduzca un número: ");
+                int number = int.Parse(Console.ReadLine());
+                if(number % 2 == 0)
+                {
+                    listaPar.Add(number);
+                }
+                else
+                {
+                    listaImpar.Add(number);
+                }
+            }
+            Console.Write("Numeros pares: ");
+            for (int i = 0; i < listaPar.Count; i++)
+            {
+                Console.Write(listaPar[i] + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Numeros impares: ");
+            for (int i = 0; i < listaImpar.Count; i++)
+            {
+                Console.Write(listaImpar[i] + " ");
+            }
+        }
+    }
+}
---
+++ Recordatorio/ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // El usuario introduce 10 números y nosotros los almacenamos en un lista con los números impares y en otra lista con números pares y mostramos al usuario el resultado de dichas listas.
+            // Fijarse en ParImpar2 de profesor.
+            List<>
+        }
+    }
+}
