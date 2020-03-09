86aca72df715b5f9d77219d2e1d485d3d5eb7a55
MameHub <61141429+MameHub@users.noreply.github.com>
2020-03-09 14:42:56 +0100

    ...

---
+++ Profesor/MultiplicationTable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduzca un número: ");
+            int number = int.Parse(Console.ReadLine());
+            int[] multiplicationTable = new int[10];
+            Console.Write("La tabla de multiplicar del número " + number + " es: ");
+            for (int i = 0; i < multiplicationTable.Length; i++)
+            {
+                multiplicationTable[i] = number * (i + 1);
+                Console.Write(multiplicationTable[i] + " ");
+            }
+        }
+    }
+}
---
+++ Recordatorio/ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Le preguntamos al usuario que tabla de multiplicar quiere ver y le mostramos la tabla de este número almacenando la tabla pedida en un array.
+            // Medio copiado de pizarra
+            
+            Console.Write("¿Que tabla de multiplicar quieres?: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            int[] multiplicar = new int[10];
+
+            for (int i = 0; i < multiplicar.Length; i++)
+            {
+                multiplicar[i] = numero * (i + 1);
+            }
+            // Está mal
+            Console.WriteLine("La tabla de multiplicar del número " + numero);
+        }
+    }
+}
