01445848b77ff9337c6635cbe61d036bae6282d4
MameHub <61141429+MameHub@users.noreply.github.com>
2020-03-10 11:07:45 +0100

    ..

--- Recordatorio/ConsoleApp11/Program.cs
+++ Recordatorio/ConsoleApp11/Program.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 
-namespace ConsoleApp10
+namespace ConsoleApp11
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // El usuario introduce un número y nosotros le mostramso la tabla de multiplicar del mismo.
+            // El usuario introduce un número y le mostramos los números impares hasta llegar a ese número.
 
             Console.Write("Introduzca un número: ");
             int x = int.Parse(Console.ReadLine());
 
-            for (int i = 1; i <= 10; i++)
+            for (int i = 1; i <= x; i += 2)
             {
-                Console.WriteLine(x + " x "  + i + " = " + x * i);
+                Console.WriteLine(i);  
             }
         }
     }
---
+++ Recordatorio/ConsoleApp12/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dibujar en pantalla con bucles anidados filas consecutivas de "x", números "1, 1 2, 1 2 3", "1, 2 2, 3 3 3" y "1 2 3 4 5".
+
+            Console.Write("Introduce el número de filas que quieras: ");
+            int rows = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("x ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            
+            int number = 1;
+            for (int i = 0; i < rows; i++, number = 1)
+            {
+                for (int j = 0; j < i + 1; j++, number++)
+                {
+                    Console.Write(number + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write((i + 1) + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int numb = 1;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < i + 1; j++, number++)
+                {
+                    Console.Write(number + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
