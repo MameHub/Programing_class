9306ff68567477693e07bf60c99b1c017619f16f
MameHub <61141429+MameHub@users.noreply.github.com>
2020-03-12 12:07:43 +0100

    .

---
+++ Objetos/ConsoleApp2/Empleado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public struct Empleado
+    {
+        public string nombre;
+        public int diaNacimiento;
+        public string mesNacimiento;
+        public int añoNacimiento;
+
+        public Empleado(string nombre, int diaNacimiento, string mesNacimiento, int añoNacimiento)
+        {
+            this.nombre = nombre;
+            this.diaNacimiento = diaNacimiento;
+            this.mesNacimiento = mesNacimiento;
+            this.añoNacimiento = añoNacimiento;
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("El nombre del empleado es: " + nombre + ".");
+            Console.WriteLine("La fecha de nacimiento del empleado es: " + diaNacimiento + " de " + mesNacimiento + " del " + añoNacimiento + ".");
+        }
+    }
+}
---
+++ Objetos/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca su nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Introduzca su día de nacimiento: ");
+            int diaNacimiento = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca su mes de nacimiento: ");
+            string mesNacimiento = Console.ReadLine();
+            Console.WriteLine("Introduzca su año de nacimiento: ");
+            int añoNacimiento = int.Parse(Console.ReadLine());
+            Empleado empleado1 = new Empleado(nombre, diaNacimiento, mesNacimiento, añoNacimiento);
+            empleado1.MostrarDatos();
+        }
+    }
+}
