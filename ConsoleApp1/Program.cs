4d38845abd9131cfbadc1bebc0a2025718b8c754
MameHub <61141429+MameHub@users.noreply.github.com>
2020-03-12 10:25:42 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,19 +5,34 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.Write("Introduzca su nombre: ");
-            string nombre = Console.ReadLine();
-            Console.Write("Introduzca una nota: ");
-            float nota = float.Parse(Console.ReadLine());
-            Alumno alumno1 = new Alumno(nombre, nota);
-            Alumno alumno2 = new Alumno("Álvaro", 7.4f);
-            Alumno alumno3 = new Alumno("Ángel", 10f);
-            Alumno alumno4 = new Alumno("Joaquín", 9.4f);
-            alumno1.MostrarDatos();
-            alumno2.MostrarDatos();
-            alumno3.MostrarDatos();
-            alumno4.MostrarDatos();
+        { 
+            //Console.Write("Introduzca su nombre: ");
+            //string nombre = Console.ReadLine();
+            //Console.Write("Introduzca una nota: ");
+            //float nota = float.Parse(Console.ReadLine());
+            //Alumno alumno1 = new Alumno(nombre, nota);
+            //Alumno alumno2 = new Alumno("Álvaro", 7.4f);
+            //Alumno alumno3 = new Alumno("Ángel", 10f);
+            //Alumno alumno4 = new Alumno("Joaquín", 9.4f);
+            //alumno1.MostrarDatos();
+            //alumno2.MostrarDatos();
+            //alumno3.MostrarDatos();
+            //alumno4.MostrarDatos();
+
+            Alumno[] alumnos = new Alumno[3];
+            for (int i = 0; i < alumnos.Length; i++)
+            {
+                Console.Write("Introduzca su nombre: ");
+                string nombre = Console.ReadLine();
+                Console.Write("Introduzca una nota: ");
+                float nota = float.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i < alumnos.Length; i++)
+            {
+                alumnos[i].MostrarDatos();
+            }
         }
     }
 }
