6cb97e6ef01d013aca0c77ce3f68c35c503f7986
MameHub <61141429+MameHub@users.noreply.github.com>
2020-03-12 10:12:04 +0100

    struct

---
+++ ConsoleApp1/Alumno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public struct Alumno
+    {
+        public string nombre;
+        public float nota;
+
+        public Alumno(string nombre, float nota)
+        {
+            this.nombre = nombre;
+            this.nota = nota;
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("El nombre del alumno es: " + nombre + ".");
+            Console.WriteLine("La nombre del nota es: " + nota + ".");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduzca su nombre: ");
+            string nombre = Console.ReadLine();
+            Console.Write("Introduzca una nota: ");
+            float nota = float.Parse(Console.ReadLine());
+        }
+    }
+}
