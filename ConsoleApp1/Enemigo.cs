b788f3a49bfc2a767fe6bdb4da73da0ee81fe22b
MameHub <61141429+MameHub@users.noreply.github.com>
2020-03-12 14:37:05 +0100

    enemy

---
+++ ConsoleApp1/Enemigo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Enemigo
+    {
+        public int vida;
+        public int maxAtk;
+        public int minAtk;
+
+        public Enemigo(int vida, int maxAtk, int minAtk)
+        {
+            this.vida = vida;
+            this.maxAtk = maxAtk;
+            this.minAtk = minAtk;
+        }
+
+        public void Atacar(Enemigo enemy)
+        {
+            Random random = new Random();
+            Enemigo.RecibirDaño(Random.Next(minAtk, maxAtk + 1));
+        }
+
+        public void RecibirDaño(int daño)
+        {
+            vida -= daño;
+            if(vida <= 0)
+            {
+                Random random = new Random();
+                Enemigo.RecibirDaño(random.Next(minAtk));
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vida = 100;
+            int maxAtk = 80;
+            int minAtk = 20;
+            Enemigo enemigo1 = new Enemigo(vida, maxAtk, minAtk);
+            Enemigo enemigo2 = new Enemigo(vida, maxAtk, minAtk);
+            while (enemigo1.vida > 0 && enemigo2.vida > 0)
+            {
+                enemigo1.Atacar(enemigo2);
+                Console.WriteLine("Vida enemigo 2: " + enemigo2.vida);
+                enemigo2.Atacar(enemigo1);
+                Console.WriteLine("Vida enemigo 1: " + enemigo1.vida);
+            }
+        }
+    }
+}
