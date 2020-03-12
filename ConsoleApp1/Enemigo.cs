5d8cf5fbacedc6f4c5de879ece22daf632a68938
MameHub <61141429+MameHub@users.noreply.github.com>
2020-03-12 14:38:27 +0100

    Update Enemigo.cs

--- ConsoleApp1/Enemigo.cs
+++ ConsoleApp1/Enemigo.cs
@@ -4,32 +4,31 @@ using System.Text;
 
 namespace ConsoleApp1
 {
-    public class Enemigo
+    public class Enemy
     {
         public int vida;
         public int maxAtk;
         public int minAtk;
 
-        public Enemigo(int vida, int maxAtk, int minAtk)
+        public Enemy(int vida, int maxAtk, int minAtk)
         {
             this.vida = vida;
             this.maxAtk = maxAtk;
             this.minAtk = minAtk;
         }
 
-        public void Atacar(Enemigo enemy)
+        public void Atacar(Enemy enemy)
         {
             Random random = new Random();
-            Enemigo.RecibirDaño(Random.Next(minAtk, maxAtk + 1));
+            enemy.RecibirDaño(random.Next(minAtk, maxAtk + 1));
         }
 
         public void RecibirDaño(int daño)
         {
             vida -= daño;
-            if(vida <= 0)
+            if (vida <= 0)
             {
-                Random random = new Random();
-                Enemigo.RecibirDaño(random.Next(minAtk));
+                Console.WriteLine("He muerto");
             }
         }
     }
