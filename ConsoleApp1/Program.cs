70ac056d7bec024852f467e1fcd1894250ad2961
MameHub <61141429+MameHub@users.noreply.github.com>
2020-03-12 10:19:37 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,14 @@ namespace ConsoleApp1
             string nombre = Console.ReadLine();
             Console.Write("Introduzca una nota: ");
             float nota = float.Parse(Console.ReadLine());
+            Alumno alumno1 = new Alumno(nombre, nota);
+            Alumno alumno2 = new Alumno("Álvaro", 7.4f);
+            Alumno alumno3 = new Alumno("Ángel", 10f);
+            Alumno alumno4 = new Alumno("Joaquín", 9.4f);
+            alumno1.MostrarDatos();
+            alumno2.MostrarDatos();
+            alumno3.MostrarDatos();
+            alumno4.MostrarDatos();
         }
     }
 }
